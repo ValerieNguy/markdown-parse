2148fa0270a8efe1b3f7c7b5d62169bca491bb7c
ValerieNguy <75493729+ValerieNguy@users.noreply.github.com>
2022-01-27 21:51:31 -0800

    code change for failed test 2 (no link in file)

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -15,22 +15,28 @@ public class MarkdownParse {
             int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
             int openParen = markdown.indexOf("(", nextCloseBracket);
             int closeParen = markdown.indexOf(")", openParen);
+            
+            
             if (nextOpenBracket != -1 && nextCloseBracket != -1 && openParen != -1 && closeParen != -1) {
                 if (nextOpenBracket == 0 || !markdown.substring(nextOpenBracket-1, nextOpenBracket).equals("!")) {
                     toReturn.add(markdown.substring(openParen + 1, closeParen));
                 }
             }
+
+            if (nextOpenBracket == -1 || nextCloseBracket == -1 || openParen == -1 || closeParen == -1) {
+                break;
+            }
+            
+            
             currentIndex = closeParen + 1;
             //System.out.println(currentIndex);
         }
         return toReturn; 
     }
     public static void main(String[] args) throws IOException {
-        
 		Path fileName = Path.of(args[0]);
 	    String contents = Files.readString(fileName);
         ArrayList<String> links = getLinks(contents);
         System.out.println(links);
-
     }
 }
